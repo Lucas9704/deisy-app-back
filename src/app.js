cb476fbb084cb62229337586187ab1dd74b910ad
Lucas <luks9@DESKTOP-CLTLFVJ>
2022-08-03 15:39:24 -0300

    message of welcome

--- src/app.js
+++ src/app.js
@@ -14,11 +14,18 @@ app.set("port", process.env.PORT || 3000);
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 //routes
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.send("<div>Welcome to the API</div>"
+		+ "<div>Version 1 Documentation in progress...</div>"
+		+ "<div>Test API v1 Get all Pets: /api/v1/pets?size=10&page=1?name=deisy </div>"
+		+ "<div>size: cant elements for page</div>"
+		+ "<div>page: page number</div>"
+		+ "<div>name: name of pet (optional)</div>"
+		
+		);
 });
 
 app.use("/api/v1", PetsRoutesV1);
